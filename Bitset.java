daa0e10317d813d1f41ed10bdfd31ad1b2d682c9
Kajal Kamble <kajalkamble045@gmail.com>
2021-12-21 16:15:23 +0530

    Update Bitset.java

--- Bitset.java
+++ Bitset.java
@@ -3,11 +3,8 @@
     {
         public static void main(String args[]) 
         {
-            BitSet obj = new BitSet(5);
-            for (int i = 0; i < 5; ++i)
-                obj.set(i);
-            obj.clear(2);
-            System.out.print(obj);
+            
+            System.out.print("Hello World!!");
         }
     }
 
